528b61ad7a84ea5bada1b5a538988cb6b2102e8b
Stanley Roberts <stanley.heronwood+git@gmail.com>
2024-05-29 20:50:19 +0100

    restructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
-import GameLayout from "./GameLayout.tsx";
+import GameLayout from "./pages/game_layout/GameLayout.tsx";
 
 window.addEventListener('resize', function(event) {
   console.log(`w: ${window.innerWidth}`)
--- src/pages/game_layout/GameLayout.tsx
+++ src/pages/game_layout/GameLayout.tsx
@@ -7,7 +7,7 @@ import WorldMap from './WorldMap';
 import YearSelection from './YearSelection';
 import GameStats from './GameStats';
 import SubmitButton from './SubmitButton';
-import { GameState } from './ApiTypes';
+import { GameState } from '../../ApiTypes';
 import { invoke } from '@tauri-apps/api';
 
 
