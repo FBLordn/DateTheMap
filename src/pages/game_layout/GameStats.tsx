4aefb5ead3a86b775e076143a8fa157d843fc8a5
LordnistLost <lordnsmaus@gmail.com>
2024-06-04 20:48:11 +0200

    Set all TS indentations to 2 spaces. (s.e.-FBL)

--- src/pages/game_layout/GameStats.tsx
+++ src/pages/game_layout/GameStats.tsx
@@ -3,28 +3,28 @@ import PublicIcon from '@mui/icons-material/Public';
 
 
 interface ListHeaderProps {
-    children?: React.ReactNode;
-    scoreRound: number[];
+  children?: React.ReactNode;
+  scoreRound: number[];
 }
 
 export default function GameStats({scoreRound, children}: ListHeaderProps) {
-    
-    return(
-        <Stack
-            direction="row"
-            display="flex"
-            justifyContent="space-evenly"
-            sx={{width:"auto", height:"auto"}}
-        >
-            <Typography>
-                Round: {scoreRound[1]}/5
-            </Typography>
-        
-            <PublicIcon />
+  
+  return(
+    <Stack
+      direction="row"
+      display="flex"
+      justifyContent="space-evenly"
+      sx={{width:"auto", height:"auto"}}
+    >
+      <Typography>
+        Round: {scoreRound[1]}/5
+      </Typography>
+  
+      <PublicIcon />
 
-            <Typography>
-                Total: {scoreRound[0]}
-            </Typography>
-        </Stack>
-    );
+      <Typography>
+        Total: {scoreRound[0]}
+      </Typography>
+    </Stack>
+  );
 }
--- src/pages/game_layout/WorldMap.tsx
+++ src/pages/game_layout/WorldMap.tsx
@@ -1,10 +1,9 @@
 import { CSSProperties } from "react"
 
 
-
 export default function WorldMap() {
-    const css:CSSProperties={"maxWidth":"100%"}
-    return (
-        <img src="src/assets/image.png" alt="tempimage" height="100%" width="auto" style={css} ></img>
-    )
+  const css:CSSProperties={"maxWidth":"100%"}
+  return (
+    <img src="src/assets/image.png" alt="tempimage" height="100%" width="auto" style={css} ></img>
+  )
 }
