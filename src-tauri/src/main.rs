4c5220becfa857a8fc2b0cf0902bcab150467e5f
LordnistLost <lordnsmaus@gmail.com>
2024-06-10 00:10:45 +0200

    Document crate. (s.e.-FBL)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-#![warn(clippy::pedantic)]
+#![warn(clippy::pedantic, missing_docs)]
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+//! Backend for Date The Map
+//!
+//! Manages the game and internal data and handles events via an API exposed to the front end
 
 use game_state::GameState;
 use std::sync::Mutex;
