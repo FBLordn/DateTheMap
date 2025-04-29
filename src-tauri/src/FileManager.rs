18694e67ef2a6055a962f26e279eaf02ae2cf98b
LordnistLost <lordnsmaus@gmail.com>
2025-04-29 23:53:33 +0200

    add os specific path locations for filemanager. (s.e.-FBL)

--- src-tauri/src/FileManager.rs
+++ src-tauri/src/FileManager.rs
@@ -1,14 +1,55 @@
+use serde::{Deserialize, Serialize};
+use std::env;
+use std::ffi::OsString;
+use std::fs;
+
+#[cfg(any(target_os = "linux", target_os = "macos"))]
+mod osfs {
+    use super::check_path_var;
+    use std::sync::LazyLock;
+    static CONFIG_PATH: LazyLock<std::ffi::OsString> =
+        LazyLock::new(|| check_path_var("XDG_CONFIG_HOME", "~/.config/".to_string()));
+    static CACHE_PATH: LazyLock<std::ffi::OsString> =
+        LazyLock::new(|| check_path_var("XDG_CACHE_HOME", "~/.cache/".to_string()));
+}
+#[cfg(target_os = "windows")]
+mod osfs {
+    use super::check_path_var;
+    use std::sync::LazyLock;
+    static CONFIG_PATH: LazyLock<std::ffi::OsString> = LazyLock::new(|| {
+        check_path_var(
+            "LOCALAPPDATA",
+            "C:\\Users\\{username}\\AppData\\Local".to_string(),
+        )
+    });
+    static CACHE_PATH: LazyLock<std::ffi::OsString> = LazyLock::new(|| {
+        check_path_var(
+            "LOCALAPPDATA",
+            "C:\\Users\\{username}\\Desktop\\".to_string(), //Punish user for missing env var
+        )
+    });
+}
+
+fn check_path_var(var: &str, alternative: String) -> OsString {
+    let path = env::var_os(var).unwrap_or(alternative.clone().into());
+    if !fs::exists(&path).unwrap_or(false) {
+        fs::create_dir(&alternative);
+        fs::create_dir(&(alternative.clone() + "DateTheMap"));
+        return alternative.into();
+    }
+    fs::create_dir(&(path.clone().into_string().unwrap() + "DateTheMap"));
+    path
+}
+
 pub enum RequestType {
     Config,
-    OfflineMap,
+    OfflineMap((u8, u8)),
 }
 
 pub struct FileManager {}
 
 impl FileManager {
-    fn executeWrite(request: RequestType, function: fn()) {}
+    fn write<T: Serialize>(request: RequestType, data: T) {}
 
-    fn executeRead(request: RequestType, function: fn() -> str) -> String {
-        return "".to_string();
-    }
+    fn read<'a, T: Deserialize<'a>>(request: RequestType) {}
 }
