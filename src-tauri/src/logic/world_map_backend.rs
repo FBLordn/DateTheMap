30c797085d0da6e600b73bfd375d9d80dee73a3b
LordnistLost <lordnsmaus@gmail.com>
2025-06-29 02:44:33 +0200

    make totally unrelated changes

--- src-tauri/src/logic/world_map_backend.rs
+++ src-tauri/src/logic/world_map_backend.rs
@@ -16,8 +16,8 @@ pub trait MapInterface {
 pub struct OHMLibrary {}
 impl MapInterface for OHMLibrary {
     fn get_raw_map(&self) -> InterfaceReturn {
-        let correct: i16 = rand::thread_rng().gen_range(MINIMUM_YEAR..=MAXIMUM_YEAR);
-        let embed: String = format!("<iframe width=\"100%\" height=\"100%\" src=\"http://127.0.0.1:3456/#map=4/49.84/14.94&layers=O&date={correct}-01-01&bbox=-43,17,73,70\"> </iframe>");
+        let correct = rand::rng().random_range(MINIMUM_YEAR..=MAXIMUM_YEAR);
+        let embed = format!("<iframe widthi=\"100%\" height=\"100%\" src=\"http://127.0.0.1:3456/#map=4/49.84/14.94&layers=O&date={correct}-01-01&bbox=-43,17,73,70\"> </iframe>");
         InterfaceReturn {
             html: embed,
             correct_year: correct,
