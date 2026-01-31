1b3bd488dcf37d1aa41ca8f5d9126bf1e0b031bf
Nils S. <nulls@mail.uni-paderborn.de>
2026-01-31 18:55:51 +0100

    add stan as consultant to credits. (s.e.-FBL)

--- src/pages/Information.tsx
+++ src/pages/Information.tsx
@@ -1,4 +1,4 @@
-import { Button, Divider, Stack, Typography } from "@mui/material";
+import { Box, Button, Divider, Stack, Typography } from "@mui/material";
 import PrefLine from "../components/PrefLine";
 
 interface InformationProps {
@@ -6,28 +6,37 @@ interface InformationProps {
 }
 
 export default function Information({onApply} : InformationProps) {
-    return <Stack height="100vh" width="100vw">
+  return (
+    <Stack height="100vh" width="100vw">
       <Typography variant='h1'>
         {"Credits"}
       </Typography>
-      <Stack 
-        spacing={3}
-        margin={4}
-        direction="column" sx={{ alignItems: 'center', mb: 1}}
-        divider={<Divider flexItem orientation="horizontal"/>}
+      <Box
+        overflow={'auto'}
+        sx={{height:"100vh"}}
       >
-        <PrefLine title="Creator" sx={{width:1, justifyContent:"space-between", paddingRight:5}}>
-          <a href='https://github.com/FBLordn' color="inherit" target="_blank" rel="noopener"> <Typography variant="h5"> {"FBLordn"} </Typography> </a>
-        </PrefLine>
-        <PrefLine title="Source" sx={{width:1, justifyContent:"space-between", paddingRight:5}}>
-          <a href='https://www.openhistoricalmap.org/' color="inherit" target="_blank" rel="noopener"> <Typography variant="h5"> {"OpenHistoricalMap"} </Typography> </a>
-        </PrefLine>
-        <PrefLine title="Logo Design" sx={{width:1, justifyContent:"space-between", paddingRight:5}}>
-          <Typography variant="h5"> {"Jarenissen"} </Typography>
-        </PrefLine>
-        <Button onClick={onApply} color="primary" variant="contained" sx={{minWidth:1/4, maxWidth:1/3, alignSelf:'center'}}>
-          {"Main Menu"}
-        </Button>
-      </Stack>
+        <Stack 
+          spacing={3}
+          margin={4}
+          direction="column" sx={{ alignItems: 'center', mb: 1}}
+          divider={<Divider flexItem orientation="horizontal"/>}
+        >
+          <PrefLine title="Creator" sx={{width:1, justifyContent:"space-between", paddingRight:5}}>
+            <a href='https://github.com/FBLordn' color="inherit" target="_blank" rel="noopener"> <Typography variant="h5"> {"FBLordn"} </Typography> </a>
+          </PrefLine>
+          <PrefLine title="Source" sx={{width:1, justifyContent:"space-between", paddingRight:5}}>
+            <a href='https://www.openhistoricalmap.org/' color="inherit" target="_blank" rel="noopener"> <Typography variant="h5"> {"OpenHistoricalMap"} </Typography> </a>
+          </PrefLine>
+          <PrefLine title="Logo Design" sx={{width:1, justifyContent:"space-between", paddingRight:5}}>
+            <Typography variant="h5"> {"Jarenissen"} </Typography>
+          </PrefLine>
+          <PrefLine title="Consultant" sx={{width:1, justifyContent:"space-between", paddingRight:5}}>
+            <a href='https://github.com/StanleyRoberts' color="inherit" target="_blank" rel="noopener"> <Typography variant="h5"> {"Stan"} </Typography> </a>
+          </PrefLine>
+        </Stack>
+      </Box>
+      <Button onClick={onApply} color="primary" variant="contained" sx={{minWidth:1/4, maxWidth:1/3, alignSelf:'center', margin:3}}>
+        {"Main Menu"}
+      </Button>
     </Stack>
-}
+)}
